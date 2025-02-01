40ef4dd327c684d84d5e2c950c8aaadbfe7280ce
Ivan Zorin <i.zorin@geryon.space>
2025-02-01 15:10:38 +0300

    Update Xray ver

--- build.sh
+++ build.sh
@@ -64,7 +64,7 @@ cp x-ui.sh build/
 mv build/xui-release build/x-ui
 cd build/bin
 
-wget "https://github.com/XTLS/Xray-core/releases/download/v25.1.1/Xray-linux-${ARCH}.zip"
+wget "https://github.com/XTLS/Xray-core/releases/download/v25.1.30/Xray-linux-${ARCH}.zip"
 unzip "Xray-linux-${ARCH}.zip"
 rm -f "Xray-linux-${ARCH}.zip" geoip.dat geosite.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
