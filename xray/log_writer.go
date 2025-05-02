1031295a880bee4fd351fe7908a8eabe63ef1e2e
Ivan Zorin <i.zorin@geryon.space>
2025-05-03 00:33:34 +0300

    Update log_writer.go

--- xray/log_writer.go
+++ xray/log_writer.go
@@ -17,8 +17,10 @@ type LogWriter struct {
 
 func (lw *LogWriter) Write(m []byte) (n int, err error) {
 	crashRegex := regexp.MustCompile(`(?i)(panic|exception|stack trace|fatal error)`)
+
 	// Convert the data to a string
 	message := strings.TrimSpace(string(m))
+
 	// Check if the message contains a crash
 	if crashRegex.MatchString(message) {
 		logger.Debug("Core crash detected:\n", message)
@@ -29,32 +31,57 @@ func (lw *LogWriter) Write(m []byte) (n int, err error) {
 		}
 		return len(m), nil
 	}
-	regex := regexp.MustCompile(`^(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) \[([^\]]+)\] (.+)$`)
-	messages := strings.Split(message, "\n")
 
-	for _, msg := range messages {
+	regex := regexp.MustCompile(`^(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}\.\d{6}) \[([^\]]+)\] (.+)$`)
+	messages := strings.SplitSeq(message, "\n")
+
+	for msg := range messages {
 		matches := regex.FindStringSubmatch(msg)
 
 		if len(matches) > 3 {
 			level := matches[2]
 			msgBody := matches[3]
+			msgBodyLower := strings.ToLower(msgBody)
 
-			// Map the level to the appropriate logger function
-			switch level {
-			case "Debug":
+			if strings.Contains(msgBodyLower, "tls handshake error") ||
+				strings.Contains(msgBodyLower, "connection ends") {
 				logger.Debug("XRAY: " + msgBody)
-			case "Info":
-				logger.Info("XRAY: " + msgBody)
-			case "Warning":
-				logger.Warning("XRAY: " + msgBody)
-			case "Error":
+				lw.lastLine = ""
+				continue
+			}
+
+			if strings.Contains(msgBodyLower, "failed") {
 				logger.Error("XRAY: " + msgBody)
-			default:
-				logger.Debug("XRAY: " + msg)
+			} else {
+				switch level {
+				case "Debug":
+					logger.Debug("XRAY: " + msgBody)
+				case "Info":
+					logger.Info("XRAY: " + msgBody)
+				case "Warning":
+					logger.Warning("XRAY: " + msgBody)
+				case "Error":
+					logger.Error("XRAY: " + msgBody)
+				default:
+					logger.Debug("XRAY: " + msg)
+				}
 			}
 			lw.lastLine = ""
 		} else if msg != "" {
-			logger.Debug("XRAY: " + msg)
+			msgLower := strings.ToLower(msg)
+
+			if strings.Contains(msgLower, "tls handshake error") ||
+				strings.Contains(msgLower, "connection ends") {
+				logger.Debug("XRAY: " + msg)
+				lw.lastLine = msg
+				continue
+			}
+
+			if strings.Contains(msgLower, "failed") {
+				logger.Error("XRAY: " + msg)
+			} else {
+				logger.Debug("XRAY: " + msg)
+			}
 			lw.lastLine = msg
 		}
 	}
